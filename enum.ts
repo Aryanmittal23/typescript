3a2ce105f259e18cee605c4c18d249ca1ed615f4
Aryanmittal23 <mittalaryan932@gmail.com>
2024-04-26 22:02:18 +0530

    tsc

---
+++ enum.ts
@@ -0,0 +1,13 @@
+enum Arithmetic {
+    Add,
+    Sum,
+    Mul,
+    Div
+}
+
+function calculate(a:number,b:number,type:Arithmetic) {
+    return 1;
+}
+
+const ans = calculate(1,2,Arithmetic.Mul)
+console.log(ans)
