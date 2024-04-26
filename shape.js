4b58a0ba646ba81b11b8548931d44c23d494db5e
Aryanmittal23 <mittalaryan932@gmail.com>
2024-04-26 21:31:54 +0530

    tsc

---
+++ shape.js
@@ -0,0 +1,18 @@
+function renderShape(shape) {
+    console.log("Rendered");
+}
+function calculateArea(shape) {
+    console.log("calculated area");
+}
+renderShape({
+    radius: 10,
+    width: 10,
+    length: 10,
+    side: 10
+});
+calculateArea({
+    radius: 10,
+    width: 10,
+    length: 10,
+    side: 10
+});
