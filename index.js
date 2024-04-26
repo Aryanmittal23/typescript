5bd99668135917e87d599bd6a94ea0293c5746a6
Aryanmittal23 <mittalaryan932@gmail.com>
2024-04-26 17:24:43 +0530

    tsc

---
+++ index.js
@@ -0,0 +1,15 @@
+"use strict";
+//const fs = require("express");
+//import {usestate} from "react"
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.calculateSum = void 0;
+function calculateSum(first, second) {
+    return first + second;
+}
+exports.calculateSum = calculateSum;
+/* in C++
+function calSum(int x,int y){
+    return a+b;
+}*/
+var ans1 = calculateSum('me ', 'aryan');
+console.log(ans1);
