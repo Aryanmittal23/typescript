3fa6047ec00d00e78b562d707e0a0ef5d0a46730
Aryanmittal23 <mittalaryan932@gmail.com>
2024-04-26 17:24:52 +0530

    tsc

---
+++ index.ts
@@ -0,0 +1,14 @@
+//const fs = require("express");
+//import {usestate} from "react"
+
+export function calculateSum(first:string,second:string){
+    return first+second;
+}
+/* in C++
+function calSum(int x,int y){
+    return a+b;
+}*/
+
+var ans1=calculateSum('me','aryan');
+console.log(ans1);
+
